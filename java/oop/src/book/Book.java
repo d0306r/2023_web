af30c69da2a0430a7f85e38cfe4e82e685f5ca5f
d0306r <rlaqja63@gmail.com>
2023-04-14 11:20:37 +0900

    230414

--- java/oop/src/book/Book.java
+++ java/oop/src/book/Book.java
@@ -1,2 +1,55 @@
-package book;public class Book {
+package book;
+
+public class Book {
+    private String name;
+    private Author author;
+    private Double price;
+    private int qty = 0;
+
+    public Book(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", price=" + price +
+                ", qty=" + qty +
+                '}';
+    }
 }
